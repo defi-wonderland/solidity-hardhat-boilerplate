de1256c44fdfceea9d1401bafa0c852f9804d971
wei3erHase <wei3erhase@defi.sucks>
2021-08-10 12:59:56 +0200

    feat: adding test to verify functionality

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,7 +38,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.4',
+        version: '0.8.6',
         settings: {
           optimizer: {
             enabled: true,
