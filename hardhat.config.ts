297425a9d3286fb7e910316e164f85c290c338f4
404skillz <404skillz@protonmail.com>
2022-01-10 09:37:16 -0300

    feat: bumps compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -57,7 +57,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.7',
+        version: '0.8.11',
         settings: {
           optimizer: {
             enabled: true,
