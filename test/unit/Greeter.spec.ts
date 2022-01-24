987786cade52b397cd7041ca04ec01e8bcc96607
wei3erHase <wei3erhase@defi.sucks>
2022-01-24 19:48:18 -0300

    feat: adding smock to boilerplate (#38)
    * feat: adding smock to boilerplate
    
    * fix: yarn.lock
    
    * fix: unit tests

--- test/unit/Greeter.spec.ts
+++ test/unit/Greeter.spec.ts
@@ -1,30 +1,39 @@
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import chai, { expect } from 'chai';
+import { MockContract, MockContractFactory, smock } from '@defi-wonderland/smock';
 import { Greeter, Greeter__factory } from '@typechained';
 import { evm } from '@utils';
 
-describe('Greeter', function () {
-  let greeter: Greeter;
-  let greeterFactory: Greeter__factory;
+chai.use(smock.matchers);
+
+describe('Greeter', () => {
+  let greeter: MockContract<Greeter>;
+  let greeterFactory: MockContractFactory<Greeter__factory>;
   let snapshotId: string;
 
   before(async () => {
-    greeterFactory = (await ethers.getContractFactory('Greeter')) as Greeter__factory;
+    greeterFactory = await smock.mock<Greeter__factory>('Greeter');
     greeter = await greeterFactory.deploy('Hello, world!');
     snapshotId = await evm.snapshot.take();
   });
+
   beforeEach(async () => {
     await evm.snapshot.revert(snapshotId);
   });
-  it("Should return the new greeting once it's changed", async function () {
+
+  it('should return current greeting', async () => {
+    await greeter.setVariable('greeting', 'Hola, mundo!');
+
+    expect(await greeter.greet()).to.equal('Hola, mundo!');
+  });
+
+  it('should return the new greeting once is changed', async () => {
     expect(await greeter.greet()).to.equal('Hello, world!');
 
     await greeter.setGreeting('Hola, mundo!');
     expect(await greeter.greet()).to.equal('Hola, mundo!');
-
-    await new Promise((res) => setTimeout(res, 5000));
   });
-  it('Should revert if greeting is empty', async function () {
+
+  it('should revert if greeting is empty', async () => {
     await expect(greeter.setGreeting('')).to.be.revertedWith('EmptyGreeting');
   });
 });
