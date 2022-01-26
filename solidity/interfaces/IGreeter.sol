fb0feb2651541a3bc3f96501d2f79ff2c944046a
wei3erHase <wei3erhase@defi.sucks>
2022-01-26 09:12:49 -0300

    fix: solidity folder tree organization (#42)
    * fix: solidity folder tree organization
    
    * fix: run linter

---
+++ solidity/interfaces/IGreeter.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.4 <0.9.0;
+
+interface IGreeter {
+  event GreetingSet(string _greeting);
+
+  error EmptyGreeting();
+
+  function greeting() external returns (string memory);
+
+  function greet() external view returns (string memory);
+
+  function setGreeting(string memory _greeting) external returns (bool);
+}
