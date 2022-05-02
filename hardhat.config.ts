2b98d5b3ed04bae7f6179d2735dc955fe962aea9
Ged <0xged@protonmail.com>
2022-05-02 14:49:20 -0300

    fix: simplified pragma version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -46,7 +46,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.11',
+        version: '0.8.7',
         settings: {
           optimizer: {
             enabled: true,
--- solidity/contracts/Greeter.sol
+++ solidity/contracts/Greeter.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity >=0.8.4 <0.9.0;
+pragma solidity 0.8.7;
 
 import 'hardhat/console.sol';
 import '../interfaces/IGreeter.sol';
