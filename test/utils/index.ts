a4de40078ee7bff6f15f9d351034934d9798e30f
Ged <0xged@protonmail.com>
2022-07-05 10:45:36 -0300

    chore: remove unecessary util

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -2,8 +2,7 @@ import * as behaviours from './behaviours';
 import * as contracts from './contracts';
 import * as erc20 from './erc20';
 import * as evm from './evm';
-import * as uniswap from './uniswap';
 import * as bn from './bn';
 import * as wallet from './wallet';
 
-export { contracts, behaviours, bn, erc20, evm, uniswap, wallet };
+export { contracts, behaviours, bn, erc20, evm, wallet };
